2c3ed71a3661707954bb30947e2db84a003db587
quinncombs <qcombs31@gmail.com>
2024-02-05 21:34:27 -0500

    update components and add servicesjsx

---
+++ client/src/components/ServicePage.jsx
@@ -0,0 +1,12 @@
+
+const Services = () => {
+    return (
+        <header className='App-header'>
+            <ul>
+                <li>service names here</li>
+            </ul>
+        </header>
+    )
+}
+
+export default Services;
--- client/src/services.jsx
+++ client/src/services.jsx
@@ -1,8 +1,9 @@
-// landing page
+//page rendering different servies
 import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
 import './index.css'
+import Services from './components/ServicePage'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-<App />
-)
+    <Services />
+    )
+    
