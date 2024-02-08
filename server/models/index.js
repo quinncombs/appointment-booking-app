e1e5550dba8b0a8c25dcf471176ea5ef38aa2873
jcameruca <jcameruca@gmail.com>
2024-02-08 16:44:57 -0700

    booking js

--- server/models/index.js
+++ server/models/index.js
@@ -2,5 +2,6 @@ const User = require('./User');
 const Product = require('./Product');
 const Category = require('./Category');
 const Order = require('./Order');
+const Booking = require('./booking')
 
 module.exports = { User, Product, Category, Order };
